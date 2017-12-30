85accdfd6cfc78ce7edd8ee3107a9359ee219cf6
mahendran <mahendran@Mahii.local>
2017-12-30 13:11:53 +0530

    Initializing Git Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fibo2048::Application.config.secret_key_base = 'e1f7c2f2089ad4a042da9fed2d773d1fa39cf89cc1ca7e1c8e8b7850d1a3fa56fa3b653123811a55a45795a58117dfb616b6c88b04d1f3786b8ab4d33adec2f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fibo2048::Application.config.session_store :cookie_store, key: '_fibo_2048_session'
